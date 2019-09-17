7cd6d467f127b9ffb8110e91faf30c16f6819450
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-16 23:27:04 -0700

    creating weather component

---
+++ src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Weather extends React.Component {
+    render() {
+        return(
+            <div>Form Component</div>
+        );
+    }
+}
+
+export default Weather;
