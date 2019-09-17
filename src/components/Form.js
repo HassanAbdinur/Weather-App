2c679d86c9eb80ea7c1adf63083366613e4bc955
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-16 23:06:39 -0700

    creating a form component

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { directive } from "@babel/types";
+
+class Form extends React.Component {
+    render() {
+        return(
+            <div>Form Component</div>
+        );
+    }
+}
+
+
+export default Form;
