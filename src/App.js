6632a91c7fcda9b9baa457c82b81e8a0af0cf2ab
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-17 00:10:39 -0700

    api call

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Weather from "./components/Weather";
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends React.Component {
+    getWeather = async () => {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    }
+
+
+
     render() {
         return(
             <div>
