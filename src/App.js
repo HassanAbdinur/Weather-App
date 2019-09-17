be772c0c6ca868b314b5c332bc407134133483dc
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-17 14:27:01 -0700

    making changes

--- src/App.js
+++ src/App.js
@@ -9,8 +9,10 @@ const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 class App extends React.Component {
     getWeather = async () => {
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        
+        const data = await api_call.json();
+        console.log("data");
     }
-
 
 
     render() {
