ea34764381677ada792d341c076fc54a7011956d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-16 23:48:07 -0700

    securing and adding api key

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
 class App extends React.Component {
     render() {
         return(
