f680e87a079ad80ceeed602dc620723e480be738
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-09-17 14:29:45 -0700

    working on inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,8 @@ class Form extends React.Component {
     render() {
         return(
             <form>
-                <input type="text" name="city" placeholder="city..." />
-                <input type="text" name="city" placeholder="city..." />
+                <input type="text" name="City" placeholder="City..." />
+                <input type="text" name="country" placeholder="country..." />
             </form>
         );
     }
